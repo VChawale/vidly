272164f15843b4de2c1156caba86215551739427
VChawale <vaibhavwchawale@gmail.com>
2021-10-09 16:09:07 -0500

    Built Movie Store component

---
+++ src/components/movies.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends React.Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = (movie) => {
+    const updatedMovies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies: updatedMovies });
+  };
+
+  render() {
+    const moviesCount = this.state.movies.length;
+    if (moviesCount === 0) {
+      return <p> There are no movies in database</p>;
+    }
+
+    return (
+      <React.Fragment>
+        <p>Showing {moviesCount} in database</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      this.handleDelete(movie);
+                    }}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movies;
